37e85f663b50be6329953e301ec4ee53948424db
GSiesto <guillermosiesto@gmail.com>
2018-02-16 18:02:57 +0000

    + User busy user free fixed + Close socket correctrly fixed ^^ + Stable working

--- ChatApplication_client.py
+++ ChatApplication_client.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*- #
+
 ##
 # University of the West of Scotland
 # Author: Guillermo Siesto Sanchez B00334584
 # Date: 2018
-# Description: The purpose of this coursework is to develop a distributed chat system using a networking library.
+# Description: The purpose of this coursework is to develop a distributed
+#   chat system using a networking library.
 #
 # ===========
 # C L I E N T
@@ -12,28 +14,32 @@
 
 ##
 # Imports
-import logging # Generate a log file to record all the changes made
+import logging      # Generate a log file to record all the changes made
 from socket import socket, AF_INET, SOCK_STREAM
 from sys import argv, stdout
-import os # For exiting of every thread
-from threading import Thread #Threading
+import os           # For exiting of every thread
+from threading import Thread    # Threading
 import threading
 
 ##
 # Config
-logging.basicConfig(level=logging.INFO, filename='log/ChatApplication_client.log', format='[%(levelname)s] %(asctime)s %(message)s', )
+logging.basicConfig(level=logging.INFO,
+                    filename='log/ChatApplication_client.log',
+                    format='[%(levelname)s] %(asctime)s %(message)s', )
 
 ##
 # Inicialization
 free = True
 kick = False
-grant = 1 # 0 = Superuser ; 1 = Normaluser
-connected = False # We must control if the conecction is on for exiting the thread if neccesary
+grant = 1               # 0 = Superuser ; 1 = Normaluser
+connected = False       # For wxiting the thread
 buffer_size = 12800
 
-# ========= LOCKS ⚩
+##
+# Locks ⚩
 send = threading.Lock()
 receive = threading.Lock()
+
 
 ##
 # Send a general message to the common room
@@ -43,81 +49,89 @@ def send_message(sock, server_ip):
             message = raw_input()
             #send.acquire() # Lock ========================== ⚩
             msg = message
-            if (msg.startswith(">")):
-                print "ZOY UN COMMANDITO HIHHI"
-                check_command(sock, msg)
-            else:
-                sock.sendall(msg)
+            sock.sendall(msg)
             #send.release() # Lock ========================== ⚩
-    except:
+    except ():
         # ^C Control
         print "] Forced exit when sending message"
+        global connected
         connected = False
         exit()
+
 
 ##
 # Receive a message thought the socket and process it
 def receive_message(sock, server_ip):
+    global connected
     try:
-        while (1):
-            #if (kick == False):
+        while (connected):
+            # if (not global kick):
             message = sock.recv(buffer_size)
             #receive.acquire() # Lock ========================== ⚩
             msg = message
             if (msg.startswith(">")):
                 print "] Command received"
                 check_command(sock, msg)
-                stdout.flush() # Clean
+                stdout.flush()      # Clean
             else:
-                if (free == True):
+                if (free):
                     print msg
-                    stdout.flush() # Clean
+                    stdout.flush()  # Clean
                 else:
-                    stdout.flush() # Clean
+                    stdout.flush()  # Clean
             #receive.release() # Lock ========================== ⚩
-    except:
+    except ():
         # ^C Control
         print "] Forced exit when receiving message"
-        connected = False
+        connected  = False
         exit()
+
 
 def check_command(sock, message):
     try:
+        global free
+        global connected
+        global kick
+        global grant
         msg = message
+        print "MESSAGE: " + msg
         if msg.startswith(">kick"):
             kick = True
             print "] You have been Kicked by an administrator"
-        elif msg.startwith(">info"):
+        elif msg.startswith(">info"):
             print "SOY UNA INFORMACION MUY BONITA"
-            stdout.flush() # Clean
+            stdout.flush()      # Clean
         elif msg.startswith(">disconnect"):
             connected = False
             print "] You have been disconnected"
-        elif msg.startwith(">busy"):
+        elif msg.startswith(">busy"):
             print "] You declared yourself as: Busy"
-            Free = False
-        elif msg.startwith(">free"):
+            free = False
+        elif msg.startswith(">free"):
             print "] You declared yourself as: Free"
-            Free = True
-        elif msg.startwith(">changegrant"):
-            if msg.startwith(">changegrant 0"):
+            free = True
+        elif msg.startswith(">changegrant"):
+            if msg.startswith(">changegrant 0"):
                 grant = 0
                 print "] Grant chages to: SuperUser"
-            elif msg.startwith(">changegrant 1"):
+            elif msg.startswith(">changegrant 1"):
                 grant = 1
                 print "] Grant chages to: Normaluser"
             else:
                 print "] ERROR with commands *changegrant* in client side"
         else:
             print "] ERROR typing commands in client side from server side"
-    except:
+    except ():
         print "] Exception on command checking"
+
 
 def exit():
     print "] Exit function"
+    global connected
     connected = False
     stdout.flush()
     os._exit(1)
+
 
 ##
 # Main
@@ -128,27 +142,29 @@ def main(argv):
     sock = socket(AF_INET, SOCK_STREAM)
 
     sock.connect((server_ip, server_port))
+    global connected
     connected = True
 
-    #============== 2 Active Threads
-    #THREADIND
-    #Thread for Sendind
+    # ============== 2 Active Threads
+    # THREADIND
+    # Thread for Sendind
     sending_t = Thread(target=send_message, args=(sock, server_ip))
     sending_t.start()
-    #Thread for Receiving
+    # Thread for Receiving
     receiving_t = Thread(target=receive_message, args=(sock, server_ip))
     receiving_t.start()
 
     try:
         while True:
-            if (connected == False):
+            if (not connected):
                 print "] Exit because disconnection"
                 exit()
-                os._exit(1) #Exit on Keyboard Interrupt
+                os._exit(1)     # Exit on Keyboard Interrupt
     except (KeyboardInterrupt, SystemExit):
         stdout.flush()
         print "] Forced exit by (KeyboardInterrupt or SystemExit) exception"
-        logging.info("] Forced exit by (KeyboardInterrupt or SystemExit) exception")
+        logging.info(
+            "] Forced exit by (KeyboardInterrupt or SystemExit) exception")
         exit()
 
 
--- ChatApplication_server.py
+++ ChatApplication_server.py
@@ -4,7 +4,8 @@
 # University of the West of Scotland
 # Author: Guillermo Siesto Sanchez B00334584
 # Date: 2018
-# Description: The purpose of this coursework is to develop a distributed chat system using a networking library.
+# Description: The purpose of this coursework is to develop a distributed chat
+#   system using a networking library.
 #
 # ===========
 # S E R V E R
@@ -15,33 +16,36 @@
 # Imports
 ##
 import datetime
-import logging # Generate a log file to record all the changes made
+import logging  # Generate a log file to record all the changes made
 import socket
 from sys import argv, stdout
-import os # For exiting of every thread
+import os   # For exiting of every thread
 import re
-from threading import Thread #Threading
+from threading import Thread    # Threading
 
 ##
 # Configuration of the log file
 ##
-logging.basicConfig(level=logging.INFO, filename='log/ChatApplication_server.log', format='[%(levelname)s] %(asctime)s %(message)s', )
+logging.basicConfig(level=logging.INFO,
+                    filename='log/ChatApplication_server.log',
+                    format='[%(levelname)s] %(asctime)s %(message)s', )
 
 ##
 # Inicialization
 ##
 host = '127.0.0.1'
 buffer_size = 12800
-n_connection = 100 # Number of possible connection
+n_connection = 100          # Number of possible connection
 
-active_connections = [] # User connected. [[username, grant, socket], ..., [username, grant, socket]]
-inactive_connections = [] # User inactive. [[username, grant, socket], ..., [username, grant, socket]]
-kick_connections = [] # Kicked users. [[username, grant, socket], ..., [username, grant, socket]]
+active_connections = []     # User connected. [[username, grant, socket], ...
+inactive_connections = []   # User inactive. [[username, grant, socket], ...
+kick_connections = []       # Kicked users. [[username, grant, socket], ...
+
 
 ##
 # First iteration between the client and the server.
 # The server will ask for the credentials of the user identifiying if it needs
-# to be registered or logged
+# to be registered or logged
 #
 # @param client_sock Raw socket introduced as a parameter
 # @param client_ip_and_port A list of two slots with the ip and the port
@@ -65,7 +69,7 @@ def connect_client(client_sock, client_ip_and_port):
     # same i think - user_name = user_name[1]
 
     if (credential_response[0] == 'y'):
-        if (credential_response[1][0] == True):
+        if (credential_response[1][0]):
             print '] USER:%s with IP:%s has join the room for the first time' %(credential_response[1][1], client_ip)
             client_sock.sendall('<Server>: You have entered the room')
             client_sock.sendall('================== < > ==================')
@@ -74,11 +78,11 @@ def connect_client(client_sock, client_ip_and_port):
             active_connections.append(user_data)
 
             try:
-                while 1:
+                while (get_socket_index(client_sock) != -1):    # While exists
                     message = client_sock.recv(buffer_size)
                     msg = message
                     check_message(client_sock, user_name, msg)
-            except:
+            except ():
                 print "] Exception when receiving on register"
                 client_exit(client_sock)
                 print "\nSever down ==================\n"
@@ -89,36 +93,37 @@ def connect_client(client_sock, client_ip_and_port):
             client_sock.sendall("<Server>: ERROR 1 -> The username CAN'T exist in the database, try again")
             connect_client(client_sock, client_ip_and_port)
 
-
     elif (credential_response[0] == 'n'):
-        if (credential_response[1][0] == True): #POINT
+        if (credential_response[1][0]):
             print '] USER:%s with IP:%s has join the room' %(credential_response[1][1], client_ip)
             client_sock.sendall('<Server>: Welcome back {} \n'.format(credential_response[1][1]))
             client_sock.sendall('================== < > ==================')
 
             with open('database/users_credentials.txt', 'r') as rDoc:
                 database_doc_list = rDoc.readlines()
-            database_doc_list = map(lambda each:each.strip("\n"), database_doc_list) # Eliminate '\n' from our list
+            database_doc_list = map(
+                                    lambda each: each.strip("\n"),
+                                    database_doc_list)  # Eliminate '\n'
 
             grant = -1
             i = 0
-            while ((i < len(database_doc_list)) and (grant < 0)): # Working correctly
-                sublist = re.split('[; #]', database_doc_list[i])
+            while ((i < len(database_doc_list)) and (grant < 0)):   # Working
+                sublist = re.split('[;#]', database_doc_list[i])
                 if (user_name == sublist[0]):
                     grant = sublist[2]
                 i = i + 1
-            rDoc.close() # Closing document
+            rDoc.close()        # Closing document
 
             # Add to the active user list
             user_data = [user_name, grant, client_sock]
             active_connections.append(user_data)
 
             try:
-                while (1): # len(active_connections) > 1 While there is an active connection try to receive #TODO Check neccesity
+                while (get_socket_index(client_sock) != -1):    # While exists
                     message = client_sock.recv(buffer_size)
                     msg = message
                     check_message(client_sock, user_name, msg)
-            except:
+            except ():
                 print "] Exception when receiving on login"
                 client_exit(client_sock)
                 print "\nSever down ==================\n"
@@ -132,6 +137,7 @@ def connect_client(client_sock, client_ip_and_port):
         print '] USER:%s with IP:%s has problems trying to connect. Please try again' %(credential_response[1][1], client_ip)
         client_sock.sendall('<Server>: You had problems to connect. Please try again')
         connect_client(client_sock, client_ip_and_port)
+
 
 ##
 # It is dedicated of asking the credential by command line to the user.
@@ -147,13 +153,13 @@ def ask_credentials(client_sock):
     try:
         client_sock.sendall('<Server>: Do you want to create a new user? [y/n]')
         response = client_sock.recv(buffer_size)
-        if (response == 'y'): #YES
-            return ('y', create_user(client_sock) )
+        if (response == 'y'):       # YES
+            return ('y', create_user(client_sock))
 
-        elif (response == 'n'): #NO
+        elif (response == 'n'):     # NO
             return ('n', login_user(client_sock))
         else:
-            #Default
+            # Default
             client_sock.sendall('<Server>: Error, you must respond with "y" saying yes or "n" saying no')
             ask_credentials(client_sock)
     except ():
@@ -184,14 +190,14 @@ def create_user(client_sock):
 
     with open('database/users_credentials.txt', 'r') as rDoc:
         database_doc_list = rDoc.readlines()
-    database_doc_list = map(lambda each:each.strip("\n"), database_doc_list) # Eliminate '\n' from our list
+    database_doc_list = map(lambda each: each.strip("\n"), database_doc_list)   # Eliminate '\n' from our list
 
     if (not(user_name == "admin") or not(user_name in database_doc_list)):
         if (user_password == user_password_2):
             with open('database/users_credentials.txt', 'a') as aDoc:
                 aDoc.write('\n' + user_name + ';' + user_password + '#' + format(1)) #TODO encrypt password
 
-            print '] %s Has join the party.\n' %user_name
+            print '] %s Has join the party.\n' % user_name
             answer = (True, user_name)
         else:
             client_sock.sendall('<Server>: The password are not the same, please try again')
@@ -202,8 +208,9 @@ def create_user(client_sock):
         answer = (False, user_name)
         ask_credentials(client_sock)
 
-    aDoc.close() # Close document
+    aDoc.close()    # Close document
     return answer
+
 
 ##
 # The user is asked to introduce his data by parameter and it will be check if
@@ -224,11 +231,11 @@ def login_user(client_sock):
     with open('database/users_credentials.txt', 'r') as rDoc:
         database_doc_list = rDoc.readlines()
 
-    database_doc_list = map(lambda each:each.strip("\n"), database_doc_list) # Eliminate '\n' from our list
+    database_doc_list = map(lambda each:each.strip("\n"), database_doc_list)    # Eliminate '\n' from our list
 
     i = 0
-    while (answer[0]==False and i < len(database_doc_list) ):
-            sublist = re.split('[; #]', database_doc_list[i]) #TODO GOOOD :)
+    while (not answer[0] and i < len(database_doc_list)):
+            sublist = re.split('[; #]', database_doc_list[i])   # TODO GOOOD :)
 
             if ((user_name == "admin") or (user_name == sublist[0])):
 
@@ -238,24 +245,24 @@ def login_user(client_sock):
                 answer = (False, user_name)
             i = i + 1
 
-    rDoc.close() # Close the document
+    rDoc.close()    # Close the document
     return answer
 
 
 def client_exit(client_sock):
-    stdout.flush()
-    print "---ALL ACTIVE CONNECTIONS:---"
+    print "\n\n]---------ALL ACTIVE CONNECTIONS:---------\n"
     print format(active_connections)
-    print "-----------------------------"
+    print "\n]-----------------------------------------\n\n"
     print "] Disconnecting: " + format(active_connections[get_socket_index(client_sock)])
     client_sock.sendall("<Server>: You are going to be disconnected by the server")
     client_sock.sendall(">disconnect")
+    stdout.flush()
 
     inactive_connections.append(active_connections[get_socket_index(client_sock)])
     active_connections.pop(get_socket_index(client_sock))
 
-    stdout.flush()
     client_sock.close()
+
 
 def clients_exit(client_sock):
     stdout.flush()
@@ -264,6 +271,7 @@ def clients_exit(client_sock):
         # Handle lists
         client_exit(active_connections[i][2])
         i += 1
+
 
 ##
 # Check if the message introduced as a parameter is of the kind of "command"
@@ -281,9 +289,10 @@ def check_message(client_sock, user_name, message):
         currentDT = datetime.datetime.now()
         # Print in every client screen
         for x in range(len(active_connections)):
-            active_connections[x][2].sendall(currentDT.strftime("%I:%M %p | ")+ user_name + ": "+ message)
+            active_connections[x][2].sendall(currentDT.strftime("%I:%M %p | ") + user_name + ": " + message)
         # Show in server side
-        print currentDT.strftime(" %I:%M %p | ")+ user_name + ": "+ message
+        print currentDT.strftime(" %I:%M %p | ") + user_name + ": " + message
+
 
 ##
 # The message will be check in case on starting with '/' (the command
@@ -294,75 +303,79 @@ def check_message(client_sock, user_name, message):
 def check_command(client_sock, user_name, message):
     msg = message
     if msg.startswith("/viewusers"):
-        print "] %s solicit /viewusers" %user_name
+        print "] %s solicit /viewusers" % user_name
         print_list(active_connections)
     elif msg.startswith("/messageto"):
-        print "] %s solicit /messageto" %user_name
+        print "] %s solicit /messageto" % user_name
         message_to(client_sock, message)
     elif msg.startswith("/changepassword"):
-        print "] %s solicit /changepassword" %user_name
+        print "] %s solicit /changepassword" % user_name
         print "e changepassword"
     elif msg.startswith("/busy"):
-        print "] %s solicit /busy" %user_name
-        client_sock.sendall(">info")
-        #client_sock.sendall(">busy")
+        print "] %s solicit /busy" % user_name
+        client_sock.sendall(">busy")
     elif msg.startswith("/free"):
-        print "] %s solicit /free" %user_name
+        print "] %s solicit /free" % user_name
         client_sock.sendall(">free")
     elif msg.startswith("/changegrant"):
-        print "] %s solicit /changegrant" %user_name
+        print "] %s solicit /changegrant" % user_name
         change_grant(client_sock, user_name, message)
     elif msg.startswith("/kickuser"):
-        print "] %s solicit /kickuser" %user_name
+        print "] %s solicit /kickuser" % user_name
         kick_user(client_sock, message)
     elif msg.startswith("/viewkickusers"):
-        print "] %s solicit /viewkickusers" %user_name
+        print "] %s solicit /viewkickusers" % user_name
         print_list(kick_connections)
     elif msg.startswith("/restart"):
-        print "] %s solicit /restart" %user_name
+        print "] %s solicit /restart" % user_name
         print "e restart"
     elif msg.startswith("/senddata"):
-        print "] %s solicit /senddata" %user_name
+        print "] %s solicit /senddata" % user_name
         print "e senddata"
     elif msg.startswith("/disconnect"):
-        print "] %s solicit /disconnect" %user_name
+        print "] %s solicit /disconnect" % user_name
         client_exit(client_sock)
     elif msg.startswith("/help"):
-        print "] %s solicit /help" %user_name
+        print "] %s solicit /help" % user_name
         client_sock.sendall("You can type:\n /viewusers\n /messageto\n /changepassword\n /busy\n /free\n /changegrant\n /kickuser\n /viewkickusers\n /restart\n /senddata\n /disconnect")
     else:
-        print "] %s solicit couldm't be resolved. Non existing commands" %user_name
+        print "] %s solicit couldm't be resolved. Non existing commands" % user_name
         client_sock.sendall("<Server>: [Error typing] Type '/help' see all possible commands")
 
+
 def message_to(client_sock, message):
-    sublist = re.split(' ', message) # It will end as ['/messageto', 'username', 'whatever', ...]
+    sublist = re.split(' ', message)    # It will end as ['/messageto', 'username', 'whatever', ...]
     index = get_user_index(sublist[1])
 
     if (index != -1):
-        del sublist[0] # Remove command
+        del sublist[0]      # Remove command
         print "LA SUBLIST HA QUEDADO:" + sublist
         active_connections[index][2].sendall("PM from: " + sublist[0])
-        del sublist[0] # Remove user_name
+        del sublist[0]      # Remove user_name
         active_connections[index][2].sendall(''.join(sublist))
     else:
         client_sock.sendall("Error 3 -> User not found")
 
+
 def kick_user(client_sock, message):
     sublist = re.split(' ', message) # It will end as ['/kickuser', 'username', 'whatever', ...]
     index = get_user_index(sublist[1])
-    if ( index != -1):
+    if (index != -1):
         active_connections[index][2].sendall(">kick")
-        user_data = [user_name, active_connections[index][1], client_sock] # Add to kick list
+        user_data = [user_name, active_connections[index][1], client_sock]      # Add to kick list
         kick_connections.append(user_data)
     else:
         client_sock.sendall("Error 3 -> User not found")
 
+
 def change_grant(client_sock, user_name, message):
     client_sock.sendall(message)
-    #TODO añadir grant al archivo de texto
+    # TODO añadir grant al archivo de texto
+
 
 ##
-# Will search in the active_connections list to look for the appearance of the username
+# Will search in the active_connections list to look for the appearance of
+# the username
 #
 # @param: user_name
 #
@@ -380,6 +393,7 @@ def get_user_index(user_name):
     else:
         return -1
 
+
 def get_socket_index(socket):
     i = 0
     encontrado = False
@@ -393,6 +407,7 @@ def get_socket_index(socket):
     else:
         return -1
 
+
 ##
 # Print a list in a formated way
 #
@@ -401,8 +416,9 @@ def print_list(list):
     print "N | User      | Grant  "
     i = 0
     while (i < len(list)):
-        print format(i) + " | "+ format(list[i][0]) + " | " + format(list[i][1])
+        print format(i) + " | " + format(list[i][0]) + " | " + format(list[i][1])
         i = i + 1
+
 
 ##
 # Main
@@ -416,17 +432,17 @@ def main(argv):
     print '] Server on port ' + str(server_port)
     logging.info("Server on port {}".format(server_port))
 
-    stdout.flush() # Clean
+    stdout.flush()      # Clean
 
     try:
         while 1:
             client_connection, addr = sock.accept()
-            print '] Client connected on '  + str(addr[0]) + ':' + str(addr[1]) +'\n'
+            print '] Client connected on '  + str(addr[0]) + ':' + str(addr[1]) + '\n'
             logging.info("Chat Client Connected on IP {} & Port {}".format(host, server_port))
 
             stdout.flush() # Clean
 
-            #THREADIND
+            # THREADIND
             server_t = Thread(target=connect_client, args=(client_connection, addr))
             server_t.start()
 
@@ -436,5 +452,6 @@ def main(argv):
         clients_exit(sock)
         print "\nSever down ==================\n"
         os._exit(1)
+
 
 main(argv)
